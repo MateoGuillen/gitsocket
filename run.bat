4b2729ae96b8ad5026e4062f9cbf9ba20d045aa0
MatiasLGonzalez <matias041198@gmail.com>
2021-08-24 01:07:42 -0400

    Arraglando .bat

--- run.bat
+++ run.bat
@@ -1,32 +1,9 @@
 @echo off
 setlocal EnableDelayedExpansion
 if exist "C:\Program Files\PostgreSQL\13\bin\psql.exe" (
-    @REM REM Copyright (c) 2012-2020, EnterpriseDB Corporation.  All rights reserved
-
-    @REM REM PostgreSQL server psql runner script for Windows
-
-    @REM SET server=localhost
-    @REM SET /P server="Server [!server!]: "
-
-    @REM SET database=postgres
-    @REM SET /P database="Database [!database!]: "
-
-    @REM SET port=5432
-    @REM SET /P port="Port [!port!]: "
-
-    @REM SET username=postgres
-    @REM SET /P username="Username [!username!]: "
-
-    @REM for /f "delims=" %%a in ('chcp ^|find /c "932"') do @ SET CLIENTENCODING_JP=%%a
-    @REM if "!CLIENTENCODING_JP!"=="1" SET PGCLIENTENCODING=SJIS
-    @REM if "!CLIENTENCODING_JP!"=="1" SET /P PGCLIENTENCODING="Client Encoding [!PGCLIENTENCODING!]: "
-
-    REM Run psql
-    @echo Intentando ingresar con las opciones por defecto, ingrese la contraseña principal:
+    @REM Run psql
+    @echo Intentando ingresar con las opciones por defecto, ingrese la contraseña principal (si estas no son sus opciones, modifique este archivo):
     "C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -U postgres -p 5432 -c "CREATE DATABASE bd_hospital;" -f "bd.sql"
 
     pause
-
-
-
 )
